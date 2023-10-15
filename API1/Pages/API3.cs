9a2d61c852d379ffd27b3071b56a893c270245cc
Ян Ходневич <yankhodnevitsch@gmail.com>
2023-10-15 22:09:54 +0300

    #3 and #4 API

---
+++ API1/Pages/API3.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+[Route("api/predictions")]
+[ApiController]
+public class PredictionController : ControllerBase
+{
+    private List<UserData> UserDataList;
+    private List<int> totalOnlineUsersList;
+
+    public PredictionController()
+    {
+        string filePath = "C:\\Users\\user\\source\\repos\\IntegrationTests\\IntegrationTests\\Data.txt";
+        UserDataList = ParseTextFile(filePath);
+
+        // Extract "Total online users" values
+        totalOnlineUsersList = UserDataList.Select(userData => userData.TotalOnlineUsers).ToList();
+    }
+    private readonly Random random = new Random();
+
+    [HttpGet("users")]
+    public IActionResult PredictUsers([FromQuery] string date)
+    {
+        int minTotalOnlineUsers = totalOnlineUsersList.Min();
+        int maxTotalOnlineUsers = totalOnlineUsersList.Max();
+
+        if (DateTime.TryParseExact(date, "yyyy-dd-MM-HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime targetDate))
+        {
+            DateTime targetDateOnly = targetDate.Date;
+
+            var relevantData = UserDataList
+                .Where(userData => userData.Time.Date == targetDateOnly)
+                .ToList();
+
+            if (relevantData.Count > 0)
+            {
+                return Ok(new { onlineUsers = random.Next(minTotalOnlineUsers, maxTotalOnlineUsers + 1) });
+            }
+        }
+        int randomOnlineUsers = random.Next(minTotalOnlineUsers, maxTotalOnlineUsers + 1);
+
+        return Ok(new { onlineUsers = randomOnlineUsers });
+    }
+
+
+    private List<UserData> ParseTextFile(string filePath)
+    {
+        List<UserData> userDataList = new List<UserData>();
+
+        string[] lines = System.IO.File.ReadAllLines(filePath);
+
+        foreach (string line in lines)
+        {
+            if (line.Contains("Total online users:"))
+            {
+                string[] parts = line.Split(';');
+                if (parts.Length == 2)
+                {
+                    UserData userData = new UserData();
+                    userData.Time = DateTime.ParseExact(parts[0].Trim(), "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                    string totalOnlineUsersString = parts[1].Trim().Replace("Total online users:", "").Trim();
+                    if (int.TryParse(totalOnlineUsersString, out int totalOnlineUsers))
+                    {
+                        userData.TotalOnlineUsers = totalOnlineUsers;
+                    }
+                    userDataList.Add(userData);
+                }
+            }
+        }
+
+        return userDataList;
+    }
+
+    public class UserData
+    {
+        public DateTime Time { get; set; }
+        public int TotalOnlineUsers { get; set; }
+    }
+}
---
+++ API2/Pages/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using static PredictionController;
+
+[Route("api/")]
+[ApiController]
+public class PredictionController : ControllerBase
+{
+    private List<UserRecord> UserDataList;
+
+    public PredictionController()
+    {
+        string filePath = "C:\\Users\\user\\source\\repos\\TDD\\TDD2\\Data2.txt";
+        UserDataList = ParseTextFile1(filePath);
+    }
+
+    [HttpGet("predictions/user")]
+    public IActionResult PredictUserOnline([FromQuery] string date, [FromQuery] double tolerance, [FromQuery] Guid userId)
+    {
+        if (UserDataList.Count == 0)
+        {
+            return NotFound("No historical data available.");
+        }
+
+        if (DateTime.TryParseExact(date, "yyyy-dd-MM-HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime targetDate))
+        {
+            // Filter historical data to include only data for the same weekday and time
+            var userRecords = UserDataList
+                .Where(record => record.UserId == userId && record.Time.DayOfWeek == targetDate.DayOfWeek && record.Time.Hour == targetDate.Hour)
+                .ToList();
+
+            if (userRecords.Count > 0)
+            {
+                // Calculate the chance based on the specified formula
+
+                double wasOnlineAtThisWeekdayAtThisTime = userRecords.Count(record => record.WasUserOnline == "online");
+                double totalWeeks = userRecords.Count;
+                double onlineChance = wasOnlineAtThisWeekdayAtThisTime / totalWeeks;
+
+                // Determine if the user will be online based on the tolerance
+                bool willBeOnline = onlineChance > tolerance;
+
+                return Ok(new { willBeOnline, onlineChance });
+            }
+            else
+            {
+                return NotFound("No historical data available for the specified user, weekday, and time.");
+            }
+        }
+
+        return BadRequest("Invalid date format.");
+    }
+
+
+
+
+
+
+    private List<UserRecord> ParseTextFile1(string filePath)
+    {
+        List<UserRecord> userDataList = new List<UserRecord>();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] parts = line.Split(';');
+                if (parts.Length == 4)
+                {
+                    UserRecord userData = new UserRecord();
+
+                    foreach (string part in parts)
+                    {
+                        if (part.StartsWith("ID:"))
+                        {
+                            userData.UserId = Guid.Parse(parts[0].Split(':')[1].Trim());
+                        }
+                        else if (part.StartsWith("Time:"))
+                        {
+                            userData.Time = DateTime.ParseExact(part.Substring(5), "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                        }
+
+
+                        else if (part.StartsWith("wasUserOnline:"))
+                        {
+                            userData.WasUserOnline = part.Substring(14);
+                        }
+                    }
+
+                    userDataList.Add(userData);
+                }
+            }
+        }
+
+        return userDataList;
+    }
+
+    private DateTime? ParseDateTimeFromPart(string part, string prefix)
+    {
+        string timestamp = part.Substring(prefix.Length).Trim();
+        if (!string.IsNullOrEmpty(timestamp))
+        {
+            if (DateTime.TryParseExact(timestamp, /*"dd.MM.yyyy HH:mm:ss"*/ "yyyy-MM-dd-HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+            {
+                return result;
+            }
+        }
+        return null;
+    }
+
+    public class UserRecord
+    {
+        public Guid UserId { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime NearestOnlineTime { get; set; }
+        public string? WasUserOnline { get; set; }
+    }
+}
